058d8ac5b7b134d40278e43388967c405157f995
[Obed Baltodano] <obedbaltodano23@homtail.com>
2024-02-19 22:08:25 -0600

    new changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 
 //burger menu
 document.addEventListener('DOMContentLoaded', function() {
-  var navMenu = document.querySelector('.nav-menu');
-  var navbarToggler = document.querySelector('.navbar-toggler');
+  let navMenu = document.querySelector('.nav-menu');
+  let navbarToggler = document.querySelector('.navbar-toggler');
 
   navMenu.style.display = 'none';
 
@@ -15,4 +15,55 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+//scroll up button
+document.addEventListener("DOMContentLoaded", function() {
+    let scrollUp = document.getElementById("scroll-up");
+    
+    scrollUp.addEventListener("click", function() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      }
+     
+      
+      
+      );
+    });
+  });
 
+  window.addEventListener("scroll", function() {
+    let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+    let scrollUp = document.getElementById("scroll-up");
+  
+    if (scrollPosition > 100) {
+      scrollUp.style.opacity = "1";
+    } else {
+     scrollUp.style.opacity = "0";
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const text = "<Full Stack Web Developer>";
+    const speed = 100;
+    const div = document.getElementById("text");
+    let index = 0;
+    let interval;
+
+    function mostrarTextoLetraPorLetra() {
+        if (index < text.length) {
+            div.textContent += text[index];
+            index++;
+        } else {
+            clearInterval(interval); // Detener la ejecución continua
+            setTimeout(function() {
+                div.textContent = ""; // Limpiar el contenido del div
+                index = 0; // Reiniciar el índice
+                interval = setInterval(mostrarTextoLetraPorLetra, speed);
+            }, 5000); // Esperar 5 segundos antes de reiniciar
+        }
+    }
+
+    // Iniciar el intervalo inicial
+    interval = setInterval(mostrarTextoLetraPorLetra, speed);
+});
